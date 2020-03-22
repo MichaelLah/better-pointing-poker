11cb47df2b3a763c599aa9c8e43c92694e3ff8c0
Michael Lah <42075214+MichaelLah@users.noreply.github.com>
2020-03-22 17:49:14 -0400

    Websockets (#1)
    * add prettier
    
    * use var for port
    
    * ok the websockets do something
    
    * websockets wip
    
    * join session component
    
    * clean up

--- App.js
+++ App.js
@@ -1,15 +1,51 @@
-const express = require('express')
-const path = require('path')
-const app = express()
+const express = require("express");
+const path = require("path");
+const app = express();
+const { Server } = require("ws");
+const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'build')))
+const projectPath = process.env.NODE_ENV === "dev" ? "public" : "build";
 
-app.get('/ping', (req, res) => {
-  return res.send('pong')
-})
+app.use(express.static(path.join(__dirname, projectPath)));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+app.get("/", (req, res) => {
+  console.log(process.env);
 
-app.listen(process.env.PORT || 8080)
+  const fileToSend = process.env.NODE_ENV === "dev" ? "index.js" : "index.html";
+  console.log({ fileToSend, projectPath });
+  res.sendFile(path.join(__dirname, projectPath, fileToSend));
+});
+
+app.get("/file", (req, res) => {
+  res.sendFile(path.join(__dirname, "src", "logo.svg"));
+});
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
+/*
+  WEBSOCKET SERVER STUFF
+ */
+const wss = new Server({ port: 5001 });
+
+wss.on("connection", ws => {
+  console.log("client connected!");
+
+  ws.on("message", data => {
+    const jsonMessage = JSON.parse(data)
+    console.log("got a message!!!!!!!!!!");
+    console.log(jsonMessage);
+    sendAllMessages('hi everyone')
+  });
+  ws.on("close", () => console.log("DC!"));
+});
+
+// setInterval(() => {
+//   wss.clients.forEach(client => {
+//     client.send(new Date().toTimeString());
+//   });
+// }, 1000);
+
+const sendAllMessages = message => {
+  wss.clients.forEach(client => {
+    client.send(message);
+  });
+};
--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import LandingPage from "./LandingPage";
+const WS_HOST = "ws://localhost:5001/";
 
 function App() {
+  const [webSocket, setWebSocket] = useState(null);
+  const [name, setName] = useState("");
+  const [joined, setJoined] = useState(false);
+
+  const joinSession = () => {
+    setJoined(true);
+  };
+  useEffect(() => {
+    setWebSocket(new WebSocket(WS_HOST));
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +22,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!joined && <LandingPage setName={setName} joinSession={joinSession} />}
+
+        <button
+          onClick={() =>
+            webSocket.send(JSON.stringify({ thing: "hi", two: "y eet" }))
+          }
+        >test send data</button>
       </header>
     </div>
   );
